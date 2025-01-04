27c1cc5e22a1671ef0677049cddeb67967a9fe33
iammeghana@proton.me <iammeghana@proton.me>
2025-01-05 00:04:48 +0530

    multiple variables in a route

--- app.py
+++ app.py
@@ -47,6 +47,37 @@ def greeting(user_id):
 
 #request.args is a dictionary 
 
+#multiple variables in a route
+#made comment_id optional that route will default to 1
+@app.route('/post/<int:post_id>/comment/', methods=['GET'])
+@app.route('/post/<int:post_id>/comment/<int:comment_id>', methods=['GET'])
+def show_comment(post_id, comment_id=None):
+    # Check for query parameters or use default value for comment_id
+    comment_id = request.args.get('comment_id', comment_id or 1, type=int)
+    
+    return f'Post ID: {post_id}, Comment ID: {comment_id}'
+#/post/10/comment/5?comment_id=20
+#comment_id = 20 (from query string).
+
+#/post/10/comment/5
+#comment_id = 5 (from path).
+
+#/post/10/comment/5?comment_id=
+#comment_id = 5 (from path; query string is empty).
+
+#/post/10/comment/5?comment_id=0
+#comment_id = 0 (from query string).
+
+#/post/10/comment/0
+#Post ID: 10, Comment ID: 1
+
+
+#You can also define default values for variables if needed    
+@app.route('/product/<int:product_id>', defaults={'comment_id': 0})
+@app.route('/product/<int:product_id>/comment/<int:comment_id>')
+def show_product(product_id, comment_id): #this is the endpoint?
+    return f'product ID: {product_id}, Comment ID: {comment_id}'
+
 
 
 if __name__ == '__main__':
