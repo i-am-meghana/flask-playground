bbe38abf817958a9fb80fa7db52a5c5de3a27a25
iammeghana@proton.me <iammeghana@proton.me>
2025-01-06 00:33:51 +0530

    get_json

--- app.py
+++ app.py
@@ -84,6 +84,19 @@ def show_product(product_id, comment_id): #this is the endpoint?
 def hello():
     return jsonify({"message": "Hello, World!"})
 
+#Create a Flask route that accepts a POST request and stores the received data in a Python dictionary.
+#Create a route @app.route('/submit', methods=['POST'])
+#Accept the data as JSON
+#Return a JSON response indicating success.
+#Test using Postman by sending a JSON payload
+data_store = []
+@app.route('/submit', methods=['POST'])
+def submit_data():
+    data = request.get_json()#convert the JSON into a Python dictionary
+    data_store.append(data)#will add this dictionary to the data_store list
+    #Before: data_store = []
+    #After: data_store = [{"name": "Jane Smith", "age": 25, "email": "jane.smith@example.com"}]
+    return jsonify({"message": "Data submitted successfully", "data": data}), 201
 
 
 if __name__ == '__main__':
