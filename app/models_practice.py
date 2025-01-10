85111174e54e10f4e42a049d7fe49a32018bd4b5
iammeghana@proton.me <iammeghana@proton.me>
2025-01-10 23:41:06 +0530

    retrieving users

--- app/models_practice.py
+++ app/models_practice.py
@@ -4,21 +4,6 @@ from app.models import User,Book  # assuming you have a User model defined
 
 models_bp = Blueprint('models_bp', __name__)
 
-# Create a view function that inserts a new record into the User table.
-@models_bp.route('/create', methods=['POST'])#endpoint
-def create_user():#view function
-    data = request.get_json()
-    name = data.get('name')
-    email = data.get('email')
-    if not name or not email:
-        return jsonify({"message": "Name and email are required!"}), 400
-
-    new_user = User(name=name, email=email)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({"message": f"User {name} added successfully!"}), 201
-    
 
 @models_bp.route('/books', methods=['POST'])
 def create_book():
@@ -35,3 +20,25 @@ def get_books():
     return jsonify(result)
 
 
+# Create a view function that inserts a new record into the User table.
+@models_bp.route('/create', methods=['POST'])#endpoint
+def create_user():#view function
+    data = request.get_json()
+    name = data.get('name')
+    email = data.get('email')
+    if not name or not email:
+        return jsonify({"message": "Name and email are required!"}), 400
+
+    new_user = User(name=name, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": f"User {name} added successfully!"}), 201
+
+    """ retrieve all users from the database.
+        Use Postman to send a GET request to retrieve the user data.
+    """
+@models_bp.route('/user')
+def user_data():
+    users = User.query.all()
+    return jsonify([{'id': user.id, 'name': user.name, 'email': user.email} for user in users])
